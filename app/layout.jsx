6e4d643df4019eda261acce3b28eb2e2a0e5da56
taiseee <taisei7548@gmail.com>
2024-05-06 23:24:17 +0900

    add: vercel speed insights

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { Providers } from './providers'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
